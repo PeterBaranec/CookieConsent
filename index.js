5de6c12f32d2c10310566869cfde5cfdad55d02f
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-23 16:17:32 +0100

    popup cookie and close cookie element

---
+++ index.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById("modal");
+const closeBtn = document.getElementById("modal-close-btn");
+
+setTimeout(function () {
+  modal.style.display = "inline";
+}, 1500);
+
+closeBtn.addEventListener("click", function () {
+  modal.style.display = "none";
+});
