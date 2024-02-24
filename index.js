9149dc9321ebf318556598e3918a8d73ce6c5e00
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-24 15:37:39 +0100

    modal messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let modal = document.getElementById("modal");
 const closeBtn = document.getElementById("modal-close-btn");
 const cookieForm = document.getElementById("cookie-form");
+const modalText = document.getElementById("modal-text");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -12,4 +13,26 @@ closeBtn.addEventListener("click", function () {
 
 cookieForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  modalText.innerHTML = `<div class="modal-inner-loading">
+  <img src="images/loading.svg" class="loading">
+  <p id="uploadText">
+  Uploading your data to the dark web...
+  </p>
+  </div>`;
+
+  setTimeout(function () {
+    document.getElementById("uploadText").innerHTML = "Making the sale...";
+  }, 1500);
+
+  setTimeout(function () {
+    document.getElementById(
+      "modal-inner"
+    ).innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="images/pirate.gif">
+    </div>
+    `;
+  }, 3000);
 });
