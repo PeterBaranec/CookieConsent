cd173784a87d8ca2f2344cf816696d417fded9d8
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-24 12:42:03 +0100

    preventdefault

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let modal = document.getElementById("modal");
 const closeBtn = document.getElementById("modal-close-btn");
+const cookieForm = document.getElementById("cookie-form");
 
 setTimeout(function () {
   modal.style.display = "inline";
@@ -7,4 +8,8 @@ setTimeout(function () {
 
 closeBtn.addEventListener("click", function () {
   modal.style.display = "none";
+});
+
+cookieForm.addEventListener("submit", function (e) {
+  e.preventDefault();
 });
