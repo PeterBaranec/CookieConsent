f2ed08d049f64058927e77f83cdd08ad83641ef8
Peter Baranec <peter.baranec.devtech@gmail.com>
2024-02-25 23:00:37 +0100

    getting data from form

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ closeBtn.addEventListener("click", function () {
 cookieForm.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  const consentFormData = new FormData(consentForm);
+  console.log(consentFormData);
+
   modalText.innerHTML = `<div class="modal-inner-loading">
   <img src="images/loading.svg" class="loading">
   <p id="uploadText">
